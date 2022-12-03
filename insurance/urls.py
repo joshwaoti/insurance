ed9b2c02faeb2a5f7db1273d9f80897f6ed3a2c2
joshwaoti <joshwaotieno643@gmail.com>
2022-12-03 13:31:49 +0300

    remove pwa and add new logo

--- insurance/urls.py
+++ insurance/urls.py
@@ -18,13 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include, re_path
 from django.urls import re_path as url
-from mpesa.urls import mpesa_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("base.urls", namespace="base")),
-    path("", include("pwa.urls")),
-    path("mpesa/", include(mpesa_urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
